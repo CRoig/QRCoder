7d7b3f93557b1f3b1f7e03b7498030b338dc7ace
Carles Roig <carles.roig@betvictor.com>
2019-04-05 08:52:51 +0200

    Clean up repo to give Carthage support

---
+++ QRCoder/QRCoder/QRCoder/QRCoder.h
@@ -0,0 +1,18 @@
+//
+//  QRCoder.h
+//  QRCoder
+//
+//  Created by Carles Roig on 04/04/2019.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QRCoder.
+FOUNDATION_EXPORT double QRCoderVersionNumber;
+
+//! Project version string for QRCoder.
+FOUNDATION_EXPORT const unsigned char QRCoderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QRCoder/PublicHeader.h>
+
+
